bad6dd6082b1a5f4fbd4315b8bcc9c3f57f2fa17
finbarinfo <BarryFinnerty@Barrys-MacBook.local>
2016-09-25 11:57:46 -0400

    this is my first commit

---
+++ more_ruby_practice.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+puts("what is your first favorite thing?")
+thing_one = gets.strip
+
+puts("what is your second favorite thing?")
+thing_two = gets.strip
+
+puts("what is your third favorite thing?")
+thing_three = gets.strip
+
+binding.pry
+
+puts("These are a few of my favorite things: #{thing_one}, #{thing_two}, #{thing_three}")
